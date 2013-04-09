5db0cbf2e10d0a509ba2a7236459b78357d3280f
ks <ks@jspcj.com>
2013-04-09 22:33:59 +0400

    почистил все

--- SQL/create-tables.sql
+++
@@ -1,4 +0,0 @@
-create table article (id int auto_increment primary key, header text, content text);
-
-insert into article(header, content) values ('111', '111');
-insert into article(header, content) values ('222', '222');
