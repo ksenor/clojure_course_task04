c6109f2c8d138652d39ad95f76afc57a6b294555
ks <ks@jspcj.com>
2013-04-16 21:26:35 +0400

    xzxz

---
+++ dumps/src/dumps/handler.clj
@@ -0,0 +1,15 @@
+(ns dumps.handler
+  (:use compojure.core)
+  (:require [compojure.handler :as handler]
+            [compojure.route :as route]))
+
+(defroutes app-routes
+  (GET "/" [] "Hello World")
+  (route/resources "/")
+  (route/not-found "Not Found"))
+
+(def app
+  (handler/site app-routes))
+
+(def -main []
+  app)
